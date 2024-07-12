923a4ca12c5f35a61e52697bd4a90e07e7681270
Ashraf Qopiah <124946844+ashrafmo-1@users.noreply.github.com>
2024-07-12 12:11:08 +0300

    Delete Quotation_Technology/webSite directory

--- Quotation_Technology/webSite/Controllers/Projects.controls.js
+++
@@ -1,9 +0,0 @@
-// const { validationResult } = require("express-validator"); // medile ware
-const Products = require("../models/products.modele");
-
-const get_projects = async (req, res) => {
-  const Products = await Products.find(); // get users data from database
-  res.json(Products);
-};
-
-module.exports = {get_projects};
--- Quotation_Technology/webSite/Routes/Projects.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-let useController = require('../Controllers/Projects.controls');
-
-router.get('/', useController.get_projects);
-
-module.exports = router;
